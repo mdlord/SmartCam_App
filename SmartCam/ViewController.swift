495dd07200cdcde98ef17685cc57f2005f94501e
Mayank Daswani <mayankdaswani123@yahoo.com>
2018-05-11 19:15:05 -0400

    camera up and running/ snapshots + time

--- SmartCam/ViewController.swift
+++ SmartCam/ViewController.swift
@@ -7,19 +7,45 @@
 //
 
 import UIKit
+import AVKit
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate{
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //start up camera
+        let capturesession = AVCaptureSession()
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else{return}
+        
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else{return}
+        capturesession.addInput(input
+        )
+        
+        capturesession.startRunning()
+        
+        let previewlayer = AVCaptureVideoPreviewLayer(session: capturesession)
+        view.layer.addSublayer(previewlayer)
+        previewlayer.frame = view.frame
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        capturesession.addOutput(dataOutput)
+        
+        //let request = VNCoreMLRequest(model: <#T##VNCoreMLModel#>, completionHandler: <#T##VNRequestCompletionHandler?##VNRequestCompletionHandler?##(VNRequest, Error?) -> Void#>)
+        //VNImageRequestHandler(cgImage: <#T##CGImage#>, options: <#T##[VNImageOption : Any]#>).perform(request)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        print("snapshot taken", Date())
+        
+        guard let pixelbuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {retun}
+        VNImageRequestHandler(cvPixelBuffer: pixelbuffer, options: <#T##[VNImageOption : Any]#>)
+        
     }
-
-
+    
 }
 
